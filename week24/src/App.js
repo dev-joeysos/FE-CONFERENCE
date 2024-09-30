d4397894902a0dd7992552c212eb95dc396fa129
dev-joeysos <joeysos@naver.com>
2024-09-30 23:44:24 +0900

    [feat] week24-assignment

---
+++ week24/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Initiate from './page/Initiate';
+
+function App() {
+  return (
+    <div className="App"> 
+      <Initiate></Initiate>
+    </div>
+  );
+}
+
+export default App;
